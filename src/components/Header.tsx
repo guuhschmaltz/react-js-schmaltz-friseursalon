3c15ace8b39978dd7244a7e10be38d90eea8030c
Gustavo Schmaltz <gustavo.schmaltz@ZANAN.LOCAL>
2021-01-07 08:38:09 -0300

    fixed title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header(){
   return(
     <div className="header  container">
       <img src={barberLogo} alt="Logo da Barbearia" className="header__logo"></img>
-      <h1 className="header__title">Schmaltz Friseursalon | Barber Shop</h1>
+      <h1 className="header__title">Schmaltz Friseursalon</h1>
       <nav className="header__nav">
             <ul>
                 <li className="header__link"><Link to="/">Home</Link></li>
