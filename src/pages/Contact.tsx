228d213ab400f591522439a87a98460754d2fccd
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2021-01-09 00:03:42 -0300

    added: css input animations

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,16 +11,31 @@ export default function Contact() {
         <form className="form__panel container" action="mailto:guuhschmaltz@gmail.com" method="post">
             <h2 className="form__title">Contato</h2>
             <label className="form__label" htmlFor="name" >Nome completo</label>
+
+            <div className="form__textbox">
             <input className="form__input" type="text" id="name" required/>
+            <div className="form__border"></div>
+            </div>
 
             <label className="form__label" htmlFor="email" >Email</label>
-            <input className="form__input" type="email" id="email" required placeholder="seuemail@dominio.com"/>
 
+            <div className="form__textbox">
+            <input className="form__input" type="email" id="email" required placeholder="seuemail@dominio.com"/>
+            <div className="form__border"></div>
+            </div>
             <label className="form__label" htmlFor="tel">Telefone</label>
+            
+            <div className="form__textbox">
             <input className="form__input" type="tel" id="tel" required placeholder="(XX) XXXXX-XXXX"/>
+            <div className="form__border"></div>
+            </div>
 
             <label className="form__label" htmlFor="msg">Mensagem</label>
-            <textarea className="form__textarea"  id="msg" required></textarea>
+
+            <div className="form__textarea">
+            <textarea className="form__inputarea"  id="msg" required></textarea>
+            <div className="form__borderarea"></div>
+            </div>
 
             <button className="form__button" type="submit">Enviar Formulário</button>
         </form>
